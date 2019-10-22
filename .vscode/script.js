e988469c65af9314b3f70fdbd3041baa68c27303
verlitas <55808608+verlitas@users.noreply.github.com>
2019-10-22 16:07:54 -0700

    getting there

--- .vscode/script.js
+++ .vscode/script.js
@@ -7,29 +7,29 @@ var highScore = 0;
 //--------------question is stored in a variable
 var question = [
     {
-        title: "How many programming questions are there?",
+        title: "How many programming languages are there?",
         options: ["698", "674", "482", "36"],
         answer: "698"
-    }
+    },
     {
         title: "Who was the first computer programmer?",
         options: ["Dennis Ritchie", "Ada Lovelace", "Alan Turing", "Tim Berners-Lee"],
         answer: "Ada Lovelace"
 
-    }
-     {
+    },
+    {
         title: "What was the first high-level programming language",
         options: ["Plankalkul", "Java", "C", "Short Code"],
         answer: "Short Code"
 
-    }
-     {
+    },
+    {
         title: "What was the name of the first computer?",
         options: ["ENIAC", "Colossus", "Abacus", "Ada"],
         answer: "Colossus"
 
-    }
-     {
+    },
+    {
         title: "What was the name of the first automated computer compiler?",
         options: ["IBM", "ALGOL", "A-o", "Autocoder"],
         answer: "A-o"
@@ -45,29 +45,64 @@ var six74 = document.querySelector("#ansB");
 var four82 = document.querySelector("#ansC");
 var thirty6 = document.querySelector("#ansD");
 
-//---q2
-var dennis = document.querySelector("#ansA1");
-var adall = document.querySelector("#ansB1");
-var alan = document.querySelector("#ansC1");
-var tim = document.querySelector("#ansD1");
+// //---q2
+// var dennis = document.querySelector("#ansA1");
+// var adall = document.querySelector("#ansB1");
+// var alan = document.querySelector("#ansC1");
+// var tim = document.querySelector("#ansD1");
 
-//---q3
-var plank = document.querySelector("#ansA2");
-var java = document.querySelector("#ansB2");
-var c = document.querySelector("#ansC2");
-var short = document.querySelector("#ansD2");
+// //---q3
+// var plank = document.querySelector("#ansA2");
+// var java = document.querySelector("#ansB2");
+// var c = document.querySelector("#ansC2");
+// var short = document.querySelector("#ansD2");
 
-//---q4
-var eniac = document.querySelector("#ansA3");
-var coloss = document.querySelector("#ansB3");
-var abacus = document.querySelector("#ansC3");
-var ada = document.querySelector("#ansD3");
+// //---q4
+// var eniac = document.querySelector("#ansA3");
+// var coloss = document.querySelector("#ansB3");
+// var abacus = document.querySelector("#ansC3");
+// var ada = document.querySelector("#ansD3");
 
-//---q5
-var ibm = document.querySelector("#ansA4");
-var algol = document.querySelector("#ansB4");
-var ao = document.querySelector("#ansC4");
-var auto = document.querySelector("#ansD4");
+// //---q5
+// var ibm = document.querySelector("#ansA4");
+// var algol = document.querySelector("#ansB4");
+// var ao = document.querySelector("#ansC4");
+// var auto = document.querySelector("#ansD4");
+
+
+
+
+
+
+
+
+
+
+
+
+//-------------running since it loads
+hideEverything();
+function hideEverything () {
+    var q1 = document.querySelector("#box")
+    q1.style.display = "none";
+    var extra = document.querySelector("#last")
+    extra.style.display = "none";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //-------------start game
@@ -78,7 +113,7 @@ startBtn.addEventListener("click", function () {
     function setTime() {
         var timerInterval = setInterval(timerDown, 1000);
         function timerDown() {
-            secondsLeft--;
+            secLeft--;
             counter.textContent = secLeft;
 
             if (secLeft === 0) {
@@ -88,156 +123,158 @@ startBtn.addEventListener("click", function () {
             }
         }
     }
+    hideStartshow1();
+    transfer();
+    transferA();
 })
 
-//-------------hide
-var q1 = document.querySelector("#q1");
-q1.style.display = "none";
-var q2 = document.querySelector("#q2");
-q2.style.display = "none";
-var q3 = document.querySelector("#q3");
-q3.style.display = "none";
-var q4 = document.querySelector("#q4");
-q4.style.display = "none";
-var q5 = document.querySelector("#q5");
-q5.style.display = "none";
-
-
-//----------------------functions
-Show();
-function Show() {
-    var q1 = document.querySelector("#q1");
+//----------------hide start show q1
+function hideStartshow1 (){
+    var hider = document.querySelector("#spage");
+    hider.style.display = "none";
+    var hidbtn = document.querySelector("#results-form");
+    hidbtn.style.display = "none";
+    var q1 = document.querySelector("#box");
     q1.style.display = "block";
 }
 
-ans1();
-ans2();
-ans3();
-ans4();
-ans5();
 
-function ans1() {
-    document.getElementById("#ansA").onclick = function () {
-        highScore++
-        hide();
-        Show2;
-    }
-    document.getElementById("#ansB").onclick = function () {
-        highScore--;
-        secLeft--;
-        hide();
-        Show2;
-    }
-    document.getElementById("#ansC").onclick = function () {
-        highScore--;
-        secLeft--;
-        hide();
-        Show2;
-    }
-    document.getElementById("#ansD").onclick = function () {
-        highScore--;
-        secLeft--;
-        hide();
-        Show2;
-    }
+//------------------transfer the div question to the array question object
+function transfer (){
+   var qText = document.getElementById("question");
+   qText.textContent = question[0].title;
+   console.log(qText);
 }
-function ans2() {
-    document.getElementById("#ansA1").onclick = function () {
-        highScore++
-        secLeft--;
-        hide();
-        Show2;
-    }
-    document.getElementById("#ansB1").onclick = function () {
-        highScore--;
-        hide();
-        Show2;
-    }
-    document.getElementById("#ansC1").onclick = function () {
-        highScore--;
-        secLeft--;
-        hide();
-        Show2;
-    }
-    document.getElementById("#ansD1").onclick = function () {
-        highScore--;
-        secLeft--;
-        hide();
-        Show2;
-    }
+
+function transferA (){
+    var a = document.getElementById("a");
+    a.textContent = question[0].options;
 }
-function ans3() {
-    document.getElementById("#ansA2").onclick = function () {
-        highScore--;
-        secLeft--;
-        hide();
-        Show2;
-    }
-    document.getElementById("#ansB2").onclick = function () {
-        highScore--;
-        secLeft--;
-        hide();
-        Show2;
-    }
-    document.getElementById("#ansC2").onclick = function () {
-        highScore--;
-        secLeft--;
-        hide();
-        Show2;
-    }
-    document.getElementById("#ansD2").onclick = function () {
-        highScore++;
-        hide();
-        Show2;
-    }
-}
-function ans4() {
-    document.getElementById("#ansA3").onclick = function () {
-        highScore--;
-        secLeft--;
-        hide();
-        Show2;
-    }
-    document.getElementById("#ansB3").onclick = function () {
-        highScore++;
-        hide();
-        Show2;
-    }
-    document.getElementById("#ansC3").onclick = function () {
-        highScore--;
-        secLeft--;
-        hide();
-        Show2;
-    }
-    document.getElementById("#ansD3").onclick = function () {
-        highScore--;
-        secLeft--;
-        hide();
-        Show2;
-    }
-}
-function ans5() {
-    document.getElementById("#ansA4").onclick = function () {
-        highScore--;
-        secLeft--;
-        hide();
-        Show2;
-    }
-    document.getElementById("#ansB4").onclick = function () {
-        highScore--;
-        secLeft--;
-        hide();
-        Show2;
-    }
-    document.getElementById("#ansC4").onclick = function () {
-        highScore++;
-        hide();
-        Show2;
-    }
-    document.getElementById("#ansD4").onclick = function () {
-        highScore--;
-        secLeft--;
-        hide();
-        Show2;
-    }
-}
+
+
+
+// function ans1() {
+//     document.getElementById("#ansA").onclick = function () {
+//         highScore++
+//         hide();
+//         Show2;
+//     }
+//     document.getElementById("#ansB").onclick = function () {
+//         highScore--;
+//         secLeft--;
+//         hide();
+//         Show2;
+//     }
+//     document.getElementById("#ansC").onclick = function () {
+//         highScore--;
+//         secLeft--;
+//         hide();
+//         Show2;
+//     }
+//     document.getElementById("#ansD").onclick = function () {
+//         highScore--;
+//         secLeft--;
+//         hide();
+//         Show2;
+//     }
+// }
+// function ans2() {
+//     document.getElementById("#ansA1").onclick = function () {
+//         highScore++
+//         secLeft--;
+//         hide();
+//         Show2;
+//     }
+//     document.getElementById("#ansB1").onclick = function () {
+//         highScore--;
+//         hide();
+//         Show2;
+//     }
+//     document.getElementById("#ansC1").onclick = function () {
+//         highScore--;
+//         secLeft--;
+//         hide();
+//         Show2;
+//     }
+//     document.getElementById("#ansD1").onclick = function () {
+//         highScore--;
+//         secLeft--;
+//         hide();
+//         Show2;
+//     }
+// }
+// function ans3() {
+//     document.getElementById("#ansA2").onclick = function () {
+//         highScore--;
+//         secLeft--;
+//         hide();
+//         Show2;
+//     }
+//     document.getElementById("#ansB2").onclick = function () {
+//         highScore--;
+//         secLeft--;
+//         hide();
+//         Show2;
+//     }
+//     document.getElementById("#ansC2").onclick = function () {
+//         highScore--;
+//         secLeft--;
+//         hide();
+//         Show2;
+//     }
+//     document.getElementById("#ansD2").onclick = function () {
+//         highScore++;
+//         hide();
+//         Show2;
+//     }
+// }
+// function ans4() {
+//     document.getElementById("#ansA3").onclick = function () {
+//         highScore--;
+//         secLeft--;
+//         hide();
+//         Show2;
+//     }
+//     document.getElementById("#ansB3").onclick = function () {
+//         highScore++;
+//         hide();
+//         Show2;
+//     }
+//     document.getElementById("#ansC3").onclick = function () {
+//         highScore--;
+//         secLeft--;
+//         hide();
+//         Show2;
+//     }
+//     document.getElementById("#ansD3").onclick = function () {
+//         highScore--;
+//         secLeft--;
+//         hide();
+//         Show2;
+//     }
+// }
+// function ans5() {
+//     document.getElementById("#ansA4").onclick = function () {
+//         highScore--;
+//         secLeft--;
+//         hide();
+//         Show2;
+//     }
+//     document.getElementById("#ansB4").onclick = function () {
+//         highScore--;
+//         secLeft--;
+//         hide();
+//         Show2;
+//     }
+//     document.getElementById("#ansC4").onclick = function () {
+//         highScore++;
+//         hide();
+//         Show2;
+//     }
+//     document.getElementById("#ansD4").onclick = function () {
+//         highScore--;
+//         secLeft--;
+//         hide();
+//         Show2;
+//     }
+// }

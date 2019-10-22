27f9321093f0eeb3e51a675780bdc11c9422031c
verlitas <55808608+verlitas@users.noreply.github.com>
2019-10-21 19:07:50 -0700

    updating js and trying to add pic

---
+++ .vscode/script.js
@@ -0,0 +1,43 @@
+//--------------question is stored in a variable
+
+var question = [
+    {
+        title: "";
+        options: "";
+        answer: "";
+    }
+    {
+        title: ""
+        options: ""
+        answer: "";
+
+    }
+    {
+        title: ""
+        options: ""
+        answer: "";
+
+    }
+    {
+        title: ""
+        options: ""
+        answer: "";
+
+    }
+    {
+        title: ""
+        options: ""
+        answer: "";
+
+    }
+    
+]
+
+//-------------answer
+
+
+
+//-------------buttons
+var startButton = document.querySelector("#start");
+//-------------counter
+var counter = document.querySelector("");
